250be4e8b309a27d56ea6059fd74e74184bb0d22
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-11-01 14:37:23 +0800

    ci
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- test/scripts/debugCluster.sh
+++ test/scripts/debugCluster.sh
@@ -16,8 +16,15 @@ E2E_LOG_FILE_NAME="$3"
 [ ! -f "$E2E_KUBECONFIG" ] && echo "error, could not find file $E2E_KUBECONFIG " && exit 1
 echo "$CURRENT_FILENAME : E2E_KUBECONFIG $E2E_KUBECONFIG "
 
-CONTROLLER_POD_LIST=$( kubectl get pods --no-headers --kubeconfig ${E2E_KUBECONFIG}  --namespace kube-system --selector app.kubernetes.io/component=egressgateway-controller --output jsonpath={.items[*].metadata.name} )
-AGENT_POD_LIST=$( kubectl get pods --no-headers --kubeconfig ${E2E_KUBECONFIG}  --namespace kube-system --selector app.kubernetes.io/component=egressgateway-agent --output jsonpath={.items[*].metadata.name} )
+# ====modify====
+COMPONENT_NAMESPACE="kube-system"
+COMPONENT_GOROUTINE_MAX=300
+CONTROLLER_LABEL="app.kubernetes.io/component=rocktemplate-controller"
+AGENT_LABEL="app.kubernetes.io/component=rocktemplate-agent"
+
+
+CONTROLLER_POD_LIST=$( kubectl get pods --no-headers --kubeconfig ${E2E_KUBECONFIG}  --namespace ${COMPONENT_NAMESPACE} --selector ${CONTROLLER_LABEL} --output jsonpath={.items[*].metadata.name} )
+AGENT_POD_LIST=$( kubectl get pods --no-headers --kubeconfig ${E2E_KUBECONFIG}  --namespace ${COMPONENT_NAMESPACE} --selector ${AGENT_LABEL} --output jsonpath={.items[*].metadata.name} )
 [ -z "$CONTROLLER_POD_LIST" ] && echo "error, failed to find any spider controller pod" && exit 1
 [ -z "$AGENT_POD_LIST" ] && echo "error, failed to find any spider agent pod" && exit 1
 
@@ -36,8 +43,8 @@ if [ "$TYPE"x == "gops"x ] ; then
     for POD in $CONTROLLER_POD_LIST ; do
       echo ""
       echo "---------${POD}--------"
-      kubectl exec ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}  gops stats 1
-      kubectl exec ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}  gops memstats 1
+      kubectl exec ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} -- gops stats 1
+      kubectl exec ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} -- gops memstats 1
     done
 
     echo ""
@@ -45,8 +52,8 @@ if [ "$TYPE"x == "gops"x ] ; then
     for POD in $AGENT_POD_LIST ; do
       echo ""
       echo "---------${POD}--------"
-      kubectl exec ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}  gops stats 1
-      kubectl exec ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}  gops memstats 1
+      kubectl exec ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} -- gops stats 1
+      kubectl exec ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} -- gops memstats 1
     done
 
 elif [ "$TYPE"x == "detail"x ] ; then
@@ -63,43 +70,43 @@ elif [ "$TYPE"x == "detail"x ] ; then
 
     echo ""
     echo "=============== event ============== "
-    echo "------- kubectl get events -n kube-system"
-    kubectl get events -n kube-system --kubeconfig ${E2E_KUBECONFIG}
+    echo "------- kubectl get events -n ${COMPONENT_NAMESPACE}"
+    kubectl get events -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG}
 
     echo ""
-    echo "=============== egressgateway-controller describe ============== "
+    echo "=============== rocktemplate-controller describe ============== "
     for POD in $CONTROLLER_POD_LIST ; do
       echo ""
-      echo "--------- kubectl describe pod ${POD} -n kube-system"
-      kubectl describe pod ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}
+      echo "--------- kubectl describe pod ${POD} -n ${COMPONENT_NAMESPACE}"
+      kubectl describe pod ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG}
     done
 
     echo ""
-    echo "=============== egressgateway-agent describe ============== "
+    echo "=============== rocktemplate-agent describe ============== "
     for POD in $AGENT_POD_LIST ; do
       echo ""
-      echo "---------kubectl describe pod ${POD} -n kube-system "
-      kubectl describe pod ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}
+      echo "---------kubectl describe pod ${POD} -n ${COMPONENT_NAMESPACE} "
+      kubectl describe pod ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG}
     done
 
     echo ""
-    echo "=============== egressgateway-controller logs ============== "
+    echo "=============== rocktemplate-controller logs ============== "
     for POD in $CONTROLLER_POD_LIST ; do
       echo ""
-      echo "---------kubectl logs ${POD} -n kube-system"
-      kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}
-      echo "--------- kubectl logs ${POD} -n kube-system --previous"
-      kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG} --previous
+      echo "---------kubectl logs ${POD} -n ${COMPONENT_NAMESPACE}"
+      kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG}
+      echo "--------- kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} --previous"
+      kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} --previous
     done
 
     echo ""
-    echo "=============== egressgateway-agent logs ============== "
+    echo "=============== rocktemplate-agent logs ============== "
     for POD in $AGENT_POD_LIST ; do
       echo ""
-      echo "--------- kubectl logs ${POD} -n kube-system "
-      kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}
-      echo "--------- kubectl logs ${POD} -n kube-system --previous"
-      kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG} --previous
+      echo "--------- kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} "
+      kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG}
+      echo "--------- kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} --previous"
+      kubectl logs ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} --previous
     done
 
     echo ""
@@ -108,7 +115,7 @@ elif [ "$TYPE"x == "detail"x ] ; then
 
     echo ""
     echo "=============== node log  ============== "
-    KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-"egressgateway"}
+    KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-"rocktemplate"}
     KIND_NODES=$(  kind get  nodes --name ${KIND_CLUSTER_NAME} )
     [ -z "$KIND_NODES" ] && echo "warning, failed to find nodes of kind cluster $KIND_CLUSTER_NAME " || true
     for NODE in $KIND_NODES ; do
@@ -117,59 +124,63 @@ elif [ "$TYPE"x == "detail"x ] ; then
     done
 
 
-elif [ "$TYPE"x == "datarace"x ] ; then
-    LOG_MARK="WARNING: DATA RACE"
+elif [ "$TYPE"x == "error"x ] ; then
+    CHECK_ERROR(){
+        LOG_MARK="$1"
+        POD="$2"
+        NAMESPACE="$3"
 
-    CHECK_DATA_RACE(){
-      echo ""
-      echo "---------${POD}--------"
-      if kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG} | grep "${LOG_MARK}" &>/dev/null ; then
-          echo "error, data race in ${POD} !!!!!!!"
-          kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}
-          RESUTL_CODE=1
-      else
-          echo "no data race "
-      fi
+        echo ""
+        echo "---------${POD}--------"
+        if kubectl logs ${POD} -n ${NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} | grep -E -i "${LOG_MARK}" &>/dev/null ; then
+            echo "error, in ${POD}, found error, ${LOG_MARK} !!!!!!!"
+            kubectl logs ${POD} -n ${NAMESPACE} --kubeconfig ${E2E_KUBECONFIG}
+            RESUTL_CODE=1
+        else
+            echo "no error "
+        fi
     }
 
-    echo ""
-    echo "=============== egressgateway-controller data race ============== "
-    for POD in $CONTROLLER_POD_LIST ; do
-      CHECK_DATA_RACE
-    done
+    DATA_RACE_LOG_MARK="WARNING: DATA RACE"
+    LOCK_LOG_MARK="Goroutine took lock"
+    PANIC_LOG_MARK="panic .* runtime error"
 
     echo ""
-    echo "=============== egressgateway-agent data race ============== "
-    for POD in $AGENT_POD_LIST ; do
-      CHECK_DATA_RACE
+    echo "=============== check kinds of error  ============== "
+    for POD in $CONTROLLER_POD_LIST $AGENT_POD_LIST ; do
+        echo ""
+        echo "----- check data race in ${COMPONENT_NAMESPACE}/${POD} "
+        CHECK_ERROR "${DATA_RACE_LOG_MARK}" "${POD}" "${COMPONENT_NAMESPACE}"
+
+        echo ""
+        echo "----- check long lock in ${COMPONENT_NAMESPACE}/${POD} "
+        CHECK_ERROR "${LOCK_LOG_MARK}" "${POD}" "${COMPONENT_NAMESPACE}"
+
+        echo ""
+        echo "----- check panic in ${COMPONENT_NAMESPACE}/${POD} "
+        CHECK_ERROR "${PANIC_LOG_MARK}" "${POD}" "${COMPONENT_NAMESPACE}"
+
+        echo ""
+        echo "----- check gorouting leak in ${COMPONENT_NAMESPACE}/${POD} "
+        GOROUTINE_NUM=`kubectl exec ${POD} -n ${COMPONENT_NAMESPACE} --kubeconfig ${E2E_KUBECONFIG} -- gops stats 1 | grep "goroutines:" | grep -E -o "[0-9]+" `
+        if [ -z "$GOROUTINE_NUM" ] ; then
+            echo "warning, failed to find GOROUTINE_NUM in ${COMPONENT_NAMESPACE}/${POD} "
+        elif (( GOROUTINE_NUM >= COMPONENT_GOROUTINE_MAX )) ; then
+             echo "maybe goroutine leak, found ${GOROUTINE_NUM} goroutines in ${COMPONENT_NAMESPACE}/${POD} , which is bigger than default ${COMPONENT_GOROUTINE_MAX}"
+             RESUTL_CODE=1
+        fi
+
+        echo ""
+        echo "----- check pod restart in ${COMPONENT_NAMESPACE}/${POD}"
+        RESTARTS=` kubectl get pod ${POD} -n ${COMPONENT_NAMESPACE} -o wide --kubeconfig ${E2E_KUBECONFIG} | sed '1 d'  | awk '{print $4}' `
+        if [ -z "$RESTARTS" ] ; then
+            echo "warning, failed to find RESTARTS in ${COMPONENT_NAMESPACE}/${POD} "
+        elif (( RESTARTS != 0 )) ; then
+             echo "found pod restart event"
+             RESUTL_CODE=1
+        fi
+
     done
-
-elif [ "$TYPE"x == "longlock"x ] ; then
-    LOG_MARK="Goroutine took lock"
-
-    CHECK_LONG_LOCK(){
-      echo ""
-      echo "---------${POD}--------"
-      if kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG} | grep "${LOG_MARK}" &>/dev/null ; then
-          echo "error, long lock in ${POD} !!!!!!!"
-          kubectl logs ${POD} -n kube-system --kubeconfig ${E2E_KUBECONFIG}
-          RESUTL_CODE=1
-      else
-          echo "no long lock "
-      fi
-    }
-    echo ""
-    echo "=============== egressgateway-controller long lock ============== "
-    for POD in $CONTROLLER_POD_LIST ; do
-      CHECK_LONG_LOCK
-    done
-
-    echo ""
-    echo "=============== egressgateway-agent long lock ============== "
-    for POD in $AGENT_POD_LIST ; do
-      CHECK_LONG_LOCK
-    done
-
 
 else
     echo "error, unknown type $TYPE "
