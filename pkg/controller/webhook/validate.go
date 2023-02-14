262188f42f1ee5cfb658379de07bddd19a25f2e9
lou-lan <loulan@loulan.me>
2023-02-14 14:44:50 +0800

    Update validate hook

--- pkg/controller/webhook/validate.go
+++ pkg/controller/webhook/validate.go
@@ -5,19 +5,46 @@ package webhook
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-
+	"net"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	egressv1 "github.com/spidernet-io/egressgateway/pkg/k8s/apis/egressgateway.spidernet.io/v1"
 )
 
 // ValidateHook ValidateHook
 func ValidateHook() *webhook.Admission {
 	return &webhook.Admission{
 		Handler: admission.HandlerFunc(func(ctx context.Context, req webhook.AdmissionRequest) webhook.AdmissionResponse {
-			fmt.Println("webhook debug")
-			if req.Kind.Kind != "" {
-				return webhook.Allowed("skip check")
+
+			switch req.Kind.Kind {
+			case "EgressGateway":
+				if req.Name != "default" {
+					return webhook.Denied("Currently, we only support one instance of EgressGateway, " +
+						"the name of the EgressGateway must be 'default'.")
+				}
+			case "EgressGatewayPolicy":
+				policy := new(egressv1.EgressGatewayPolicy)
+				err := json.Unmarshal(req.Object.Raw, policy)
+				if err != nil {
+					return webhook.Denied(fmt.Sprintf("json unmarshal EgressGatewayPolicy with error: %v", err))
+				}
+				invalidList := make([]string, 0)
+				for _, subnet := range policy.Spec.DestSubnet {
+					ip, _, err := net.ParseCIDR(subnet)
+					if err != nil {
+						invalidList = append(invalidList, subnet)
+						continue
+					}
+					if ip.To4() == nil && ip.To16() == nil {
+						invalidList = append(invalidList, subnet)
+					}
+				}
+				if len(invalidList) > 0 {
+					return webhook.Denied(fmt.Sprintf("invalid destSubnet list: %v", invalidList))
+				}
 			}
 
 			return webhook.Allowed("checked")
--- pkg/controller/webhook/validate_test.go
+++ pkg/controller/webhook/validate_test.go
@@ -30,11 +30,21 @@ func TestValidateEgressGateway(t *testing.T) {
 			existingResources: nil,
 			newResource: &egressv1.EgressGateway{
 				ObjectMeta: metav1.ObjectMeta{
-					Name: "EgressGateway",
+					Name: "default",
 				},
 				Spec: egressv1.EgressGatewaySpec{},
 			},
 			expAllow: true,
+		},
+		"EgressGateway name not equal default": {
+			existingResources: nil,
+			newResource: &egressv1.EgressGateway{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "default-xxx",
+				},
+				Spec: egressv1.EgressGatewaySpec{},
+			},
+			expAllow: false,
 		},
 	}
 	for name, c := range cases {
@@ -45,13 +55,140 @@ func TestValidateEgressGateway(t *testing.T) {
 			validator := ValidateHook()
 			resp := validator.Handle(ctx, admission.Request{
 				AdmissionRequest: admissionv1.AdmissionRequest{
-					Name:      c.newResource.Name,
+					Name: c.newResource.Name,
+					Kind: metav1.GroupVersionKind{
+						Kind: "EgressGateway",
+					},
 					Operation: admissionv1.Create,
 					Object: runtime.RawExtension{
 						Raw: marshalledRequestObject,
 					},
 				},
 			})
+
+			assert.Equal(t, c.expAllow, resp.Allowed)
+			if c.expErrMessage != "" {
+				assert.Equal(t, c.expErrMessage, resp.AdmissionResponse.Result.Message)
+			}
+		})
+	}
+}
+
+func TestValidateEgressGatewayPolicy(t *testing.T) {
+	ctx := context.Background()
+
+	cases := map[string]struct {
+		existingResources []runtime.Object
+		destSubnet        []string
+		expAllow          bool
+		expErrMessage     string
+	}{
+		"case, valid": {
+			existingResources: nil,
+			destSubnet: []string{
+				"192.168.1.1/24",
+				"1.1.1.1/32",
+				"10.0.6.1/16",
+				"fd00::21/112",
+			},
+			expAllow: true,
+		},
+		"case1, not valid": {
+			existingResources: nil,
+			destSubnet: []string{
+				"1.1.1.1",
+			},
+			expAllow: false,
+		},
+		"case2, not valid": {
+			existingResources: nil,
+			destSubnet: []string{
+				"1.1.1.1999/24",
+			},
+			expAllow: false,
+		},
+		"case3, not valid": {
+			existingResources: nil,
+			destSubnet: []string{
+				"---",
+			},
+			expAllow: false,
+		},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+
+			policy := &egressv1.EgressGatewayPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "policy",
+				},
+				Spec: egressv1.EgressGatewayPolicySpec{
+					DestSubnet: c.destSubnet,
+				},
+			}
+
+			marshalledRequestObject, err := json.Marshal(policy)
+			assert.NoError(t, err)
+
+			validator := ValidateHook()
+			resp := validator.Handle(ctx, admission.Request{
+				AdmissionRequest: admissionv1.AdmissionRequest{
+					Name: policy.Name,
+					Kind: metav1.GroupVersionKind{
+						Kind: "EgressGatewayPolicy",
+					},
+					Operation: admissionv1.Create,
+					Object: runtime.RawExtension{
+						Raw: marshalledRequestObject,
+					},
+				},
+			})
+
+			assert.Equal(t, c.expAllow, resp.Allowed)
+			if c.expErrMessage != "" {
+				assert.Equal(t, c.expErrMessage, resp.AdmissionResponse.Result.Message)
+			}
+		})
+	}
+}
+
+func TestValidateEgressNode(t *testing.T) {
+	ctx := context.Background()
+
+	cases := map[string]struct {
+		newResource   *egressv1.EgressNode
+		expAllow      bool
+		expErrMessage string
+	}{
+		"all valid": {
+			newResource: &egressv1.EgressNode{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "node1",
+				},
+				Spec: egressv1.EgressNodeSpec{},
+			},
+			expAllow: true,
+		},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			marshalledRequestObject, err := json.Marshal(c.newResource)
+			assert.NoError(t, err)
+
+			req := admission.Request{
+				AdmissionRequest: admissionv1.AdmissionRequest{
+					Name: c.newResource.Name,
+					Kind: metav1.GroupVersionKind{
+						Kind: "EgressNode",
+					},
+					Operation: admissionv1.Create,
+					Object: runtime.RawExtension{
+						Raw: marshalledRequestObject,
+					},
+				},
+			}
+			validator := ValidateHook()
+			resp := validator.Handle(ctx, req)
 
 			assert.Equal(t, c.expAllow, resp.Allowed)
 			if c.expErrMessage != "" {
