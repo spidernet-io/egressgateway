9a751b3c600114c34a061692d3a9cd14a8321d69
lou-lan <loulan@loulan.me>
2023-04-12 16:28:09 +0800

    Update CRDs

---
+++ pkg/k8s/apis/egressgateway.spidernet.io/v1beta1/egressclusterinfo_types.go
@@ -0,0 +1,55 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package v1beta1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// EgressClusterInfoList contains a list of EgressClusterStatus
+// +kubebuilder:object:root=true
+type EgressClusterInfoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []EgressClusterInfo `json:"items"`
+}
+
+// EgressClusterInfo describes the status of cluster
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:categories={egressclusterinfo},path="egressclusterinfos",singular="egressclusterinfo",scope="Cluster",shortName={eci}
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +genclient
+// +genclient:nonNamespaced
+type EgressClusterInfo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	// +kubebuilder:validation:Optional
+	Spec EgressEndpointSliceSpec `json:"spec,omitempty"`
+	// +kubebuilder:validation:Optional
+	Status EgressClusterStatus `json:"status,omitempty"`
+}
+
+type EgressClusterStatusSpec struct{}
+
+type EgressClusterStatus struct {
+	// +kubebuilder:validation:Optional
+	EgressIgnoreCIDR EgressIgnoreCIDR `json:"egressIgnoreCIDR,omitempty"`
+}
+
+type EgressIgnoreCIDR struct {
+	// +kubebuilder:validation:Optional
+	NodeIP IPListPair `json:"nodeIP,omitempty"`
+	// +kubebuilder:validation:Optional
+	ClusterIP IPListPair `json:"clusterIP,omitempty"`
+	// +kubebuilder:validation:Optional
+	PodCIDR IPListPair `json:"podCIDR,omitempty"`
+}
+
+type IPListPair struct {
+	// +kubebuilder:validation:Optional
+	IPv4 []string `json:"ipv4,omitempty"`
+	// +kubebuilder:validation:Optional
+	IPv6 []string `json:"ipv6,omitempty"`
+}
---
+++ pkg/k8s/apis/egressgateway.spidernet.io/v1beta1/egressendpointslice_types.go
@@ -0,0 +1,56 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EgressEndpointSliceList contains a list of EgressEndpointSlice
+// +kubebuilder:object:root=true
+type EgressEndpointSliceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []EgressEndpointSlice `json:"items"`
+}
+
+// EgressEndpointSlice is a list of endpoint
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:categories={egressendpointslice},path="egressendpointslices",singular="egressendpointslice",scope="Namespace",shortName={ees}
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +genclient
+type EgressEndpointSlice struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	// +kubebuilder:validation:Optional
+	Spec EgressEndpointSliceSpec `json:"spec,omitempty"`
+	// +kubebuilder:validation:Optional
+	Status EgressEndpointSliceSpecStatus `json:"status,omitempty"`
+}
+
+type EgressEndpointSliceSpec struct{}
+
+type EgressEndpointSliceSpecStatus struct {
+	Endpoints []EgressEndpoint `json:"endpoints,omitempty"`
+}
+
+type EgressEndpoint struct {
+	// +kubebuilder:validation:Optional
+	PodName string `json:"podName,omitempty"`
+	// +kubebuilder:validation:Optional
+	IPv4List []string `json:"IPv4List,omitempty"`
+	// +kubebuilder:validation:Optional
+	IPv6List []string `json:"IPv6List,omitempty"`
+	// +kubebuilder:validation:Optional
+	NodeName string `json:"nodeName,omitempty"`
+	// +kubebuilder:validation:Optional
+	UUID string `json:"uuid,omitempty"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EgressEndpointSlice{}, &EgressEndpointSliceList{})
+}
