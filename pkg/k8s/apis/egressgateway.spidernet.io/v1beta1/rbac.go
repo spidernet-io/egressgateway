11814217a95b14f870184dde9962b4e77972e939
lou-lan <loulan@loulan.me>
2023-05-18 11:18:35 +0800

    Fix RBAC authorization that doesn't include EgressClusterEndpointSlices resource.

--- pkg/k8s/apis/egressgateway.spidernet.io/v1beta1/rbac.go
+++ pkg/k8s/apis/egressgateway.spidernet.io/v1beta1/rbac.go
@@ -1,7 +1,7 @@
 // Copyright 2022 Authors of spidernet-io
 // SPDX-License-Identifier: Apache-2.0
 
-// +kubebuilder:rbac:groups=egressgateway.spidernet.io,resources=egressgateways;egressnodes;egressclustergatewaypolicies;egressgatewaypolicies;egressendpointslices;egressclusterinfos,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=egressgateway.spidernet.io,resources=egressgateways;egressnodes;egressclustergatewaypolicies;egressgatewaypolicies;egressendpointslices;egressclusterendpointslices;egressclusterinfos,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=egressgateway.spidernet.io,resources=egressgateways/status;egressnodes/status;egressclustergatewaypolicies/status;egressgatewaypolicies/status;egressclusterinfos/status,verbs=get;update;patch
 
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;get;list;watch;update;delete
