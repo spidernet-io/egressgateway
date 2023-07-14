3c236822200b962103f3972888f37e23fde60182
lou-lan <loulan@loulan.me>
2023-07-14 17:10:24 +0800

    Add origin link and changes of vendor code

--- pkg/iptables/interface.go
+++ pkg/iptables/interface.go
@@ -14,6 +14,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Based on https://github.com/projectcalico/calico/tree/eb0e4ca98cfee2a9d968e8abdb09d72ddca6d8a5/felix/iptables
+//
+// Changes:
+// - Update log output
+// - Fix some hardcoded variables
 
 package iptables
 
