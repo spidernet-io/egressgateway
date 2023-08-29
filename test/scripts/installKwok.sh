182767f642dbbb691dad4f988c84ed2bcb6985a4
bzsuni <86399306+bzsuni@users.noreply.github.com>
2023-08-29 11:31:48 +0800

    fix install kwok (#708)
    Signed-off-by: bzsuni <bingzhe.sun@daocloud.io>

--- test/scripts/installKwok.sh
+++ test/scripts/installKwok.sh
@@ -5,47 +5,17 @@
 set -o errexit -o nounset -o pipefail
 set -x
 
-# Temporary directory
-KWOK_WORK_DIR=$(mktemp -d)
-echo "KWOK_WORK_DIR=${KWOK_WORK_DIR}"
-
 # KWOK repository
 KWOK_REPO=kubernetes-sigs/kwok
-# Get latest
-got="no"
-for i in {0..12}; do
-  KWOK_LATEST_RELEASE=$(curl "https://api.github.com/repos/${KWOK_REPO}/releases/latest" | jq -r '.tag_name')
-  if [[ -z ${KWOK_LATEST_RELEASE} ]]; then
-    echo "${i}th, can not get tag, retry..."
-    sleep 2s
-  else
-    echo "KWOK_LATEST_RELEASE=${KWOK_LATEST_RELEASE}"
-    got="ok"
-    break
-  fi
-done
 
-if [[ "${got}" = "no" ]]; then
-  echo "timeout to get tag"
-  exit 1
-fi
+# Deployment kwok and set up CRDs
+curl -x "${https_proxy}" -L https://github.com/${KWOK_REPO}/releases/download/${KWOK_VERSION}/kwok.yaml | kubectl --kubeconfig ${E2E_KIND_KUBECONFIG_PATH} apply -f - || { echo failed install kwok; exit 1; }
 
-cat <<EOF > "${KWOK_WORK_DIR}/kustomization.yaml"
-apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization
-images:
-- name: registry.k8s.io/kwok/kwok
-  newTag: "${KWOK_LATEST_RELEASE}"
-resources:
-- "https://github.com/${KWOK_REPO}/kustomize/kwok?ref=${KWOK_LATEST_RELEASE}"
-EOF
+# Set up default CRs of Stages
+curl -x "${https_proxy}" -L https://github.com/${KWOK_REPO}/releases/download/${KWOK_VERSION}/stage-fast.yaml | kubectl --kubeconfig ${E2E_KIND_KUBECONFIG_PATH} apply -f - || { echo failed install kwok; exit 1; }
 
-kubectl --kubeconfig ${E2E_KIND_KUBECONFIG_PATH} kustomize "${KWOK_WORK_DIR}" > "${KWOK_WORK_DIR}/kwok.yaml"
-
-kubectl --kubeconfig ${E2E_KIND_KUBECONFIG_PATH} apply -f "${KWOK_WORK_DIR}/kwok.yaml"
-
+# Wait pod running
 kubectl --kubeconfig ${E2E_KIND_KUBECONFIG_PATH} wait --for=condition=ready -l app=kwok-controller --timeout=${INSTALL_TIME_OUT} pod -n kube-system
 kubectl --kubeconfig ${E2E_KIND_KUBECONFIG_PATH} get po -l app=kwok-controller -n kube-system
 
 echo -e "\033[35m Succeed to install KWOK \033[0m"
-rm -rf ${KWOK_WORK_DIR}/kustomization.yaml || exit 0
