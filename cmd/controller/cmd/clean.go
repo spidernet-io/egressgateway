26fb6477f2184b1d702100e252a8e452cc541085
lou-lan <loulan@loulan.me>
2023-09-04 17:50:59 +0800

    Update uninstall process

---
+++ cmd/controller/cmd/clean.go
@@ -0,0 +1,76 @@
+// Copyright 2022 Authors of spidernet-io
+// SPDX-License-Identifier: Apache-2.0
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	webhook "k8s.io/api/admissionregistration/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/spidernet-io/egressgateway/pkg/schema"
+)
+
+var cleanCmd = &cobra.Command{
+	Use:   "clean",
+	Short: "Clean resources",
+	Long:  "Clean resources with specified parameters.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		validate, err := cmd.Flags().GetString("validate")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		mutating, err := cmd.Flags().GetString("mutating")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("validate %s\nmutating %s\n", validate, mutating)
+		err = clean(validate, mutating)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+func clean(validate, mutating string) error {
+	cfg, err := ctrl.GetConfig()
+	if err != nil {
+		return err
+	}
+
+	cli, err := client.New(cfg, client.Options{Scheme: schema.GetScheme()})
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	vObj := &webhook.ValidatingWebhookConfiguration{}
+	err = cli.Get(ctx, client.ObjectKey{Name: validate}, vObj)
+	if err == nil {
+		err := cli.Delete(ctx, vObj)
+		if err != nil {
+			return err
+		}
+	}
+
+	mObj := &webhook.MutatingWebhookConfiguration{}
+	err = cli.Get(ctx, client.ObjectKey{Name: mutating}, mObj)
+	if err == nil {
+		err := cli.Delete(ctx, mObj)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- cmd/controller/cmd/root.go
+++ cmd/controller/cmd/root.go
@@ -68,6 +68,10 @@ restart:
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	cleanCmd.Flags().String("validate", "", "Specify validate parameter")
+	cleanCmd.Flags().String("mutating", "", "Specify mutating parameter")
+
+	rootCmd.AddCommand(cleanCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
