1c449532c2d02501e701c7f75c5d60c7aed7814a
bzsuni <86399306+bzsuni@users.noreply.github.com>
2023-11-07 16:00:57 +0800

    Add case P00021 (#901)
    Signed-off-by: bzsuni <bingzhe.sun@daocloud.io>

--- test/e2e/common/pod.go
+++ test/e2e/common/pod.go
@@ -67,6 +67,34 @@ func CreatePod(ctx context.Context, cli client.Client, image string) (*corev1.Po
 	}
 }
 
+func CreatePodCustom(ctx context.Context, cli client.Client, name, image string, setUp func(pod *corev1.Pod)) (*corev1.Pod, error) {
+	var terminationGracePeriodSeconds int64 = 0
+
+	res := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: corev1.PodSpec{
+			TerminationGracePeriodSeconds: &terminationGracePeriodSeconds,
+			Containers: []corev1.Container{
+				{
+					Name:            name,
+					Image:           image,
+					ImagePullPolicy: corev1.PullIfNotPresent,
+					Command:         []string{"/bin/sh", "-c", "sleep infinity"},
+				},
+			},
+		}}
+
+	setUp(res)
+
+	err := cli.Create(ctx, res)
+	if err != nil {
+		return nil, fmt.Errorf("error:\n%w\npod yaml:\n%s\n", err, GetObjYAML(res))
+	}
+	return res, nil
+}
+
 // CreatePods create pods by gaven number "n"
 func CreatePods(ctx context.Context, cli client.Client, img string, n int) []*corev1.Pod {
 	var res []*corev1.Pod
@@ -79,4 +107,33 @@ func CreatePods(ctx context.Context, cli client.Client, img string, n int) []*co
 		i++
 	}
 	return res
+}
+
+func WaitPodRunning(ctx context.Context, cli client.Client, pod *corev1.Pod, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	var e error
+
+	for {
+		select {
+		case <-ctx.Done():
+			if e != nil {
+				return fmt.Errorf("timeout to wait the pod running, error: %v", e)
+			}
+			return fmt.Errorf("timeout to wait the pod running")
+
+		default:
+			err := cli.Get(ctx, types.NamespacedName{Namespace: pod.Namespace, Name: pod.Name}, pod)
+			if err != nil {
+				e = err
+				time.Sleep(time.Second)
+				continue
+			}
+			if pod.Status.Phase == corev1.PodRunning {
+				return nil
+			}
+			time.Sleep(time.Second)
+		}
+	}
 }
