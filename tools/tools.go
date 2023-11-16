bc7084f775d5d65a4063ad4d235cc9d44e4a2934
lou-lan <loulan@loulan.me>
2023-11-16 14:50:59 +0800

    Remove unused dependence (#987)

--- tools/tools.go
+++ tools/tools.go
@@ -11,6 +11,5 @@ import (
 	_ "github.com/onsi/ginkgo/v2"
 	// _ "github.com/gogo/protobuf/gogoproto" // Used for protobuf generation of pkg/k8s/types/slim/k8s
 	// _ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
